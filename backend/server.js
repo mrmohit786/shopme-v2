436beaa0df59e2b3f2e91c8dbf443a12ad496e71
Mohit Patel <patelmohit719@gmail.com>
2021-06-11 00:07:50 +0530

    add comments

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+/* npm modules */
 import express from 'express';
 import colors from 'colors';
 import logger from 'morgan';
@@ -9,44 +10,50 @@ import hpp from 'hpp';
 import mongoSanitize from 'express-mongo-sanitize';
 import rateLimit from 'express-rate-limit';
 
+/* local imports */
 import env from './config/env.js';
 import connectDB from './config/database.js';
 import routes from './routes/routes.js';
 import { errorHandler, notFound } from './middleware/error.js';
 
+/* connect mongo database */
 connectDB();
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-// Restrict all routes to only 100 requests per IP address every 1o minutes
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 100, // 100 requests per IP
+  max: 200, // 200 requests per IP
 });
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+/* Restrict all routes to only 200 requests per IP address every 1o minutes */
 app.use(limiter);
 
-// Using helmet middleware
+/* secure headers */
 app.use(helmet());
 
-// Protect against XSS attacks, should come before any routes
+/* Protect against XSS attacks*/
 app.use(xssClean());
 
-// Protect against HPP, should come before any routes
+/* Protect against HPP, should come before any routes */
 app.use(hpp());
 
-// Remove all keys containing prohibited characters
+/* Remove all keys containing prohibited characters */
 app.use(mongoSanitize());
 
+/* log APIs hit by client "DEVELOPMENT MODE ONLY" */
 if (env.NODE_ENV === 'development') {
   app.use(logger('dev'));
 }
 
 app.use(
   express.json({
-    // We need the raw body to verify webhook signatures.
-    // Let's compute it only when hitting the Stripe webhook endpoint.
+    /* We need the raw body to verify webhook signatures.
+       Let's compute it only when hitting the Stripe webhook endpoint. */
     verify: function (req, res, buf) {
       if (req.originalUrl.includes('/webhook')) {
         req.rawBody = buf.toString();
@@ -55,14 +62,13 @@ app.use(
   }),
 );
 
-app.use(express.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+/* Alternative of cors access
+ * app.use((req, res, next) => {
+ * res.setHeader('Access-Control-Allow-Origin', '*');
+ * res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+ * res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+ * next(); });
+ */
 
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
